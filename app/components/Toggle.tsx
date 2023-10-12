1d3b08497e07c4b84161cc0b875e5d0023aa604f
Shayan <shayanakhlaghian@gmail.com>
2023-10-12 15:34:44 +0330

    create toggle
    create toggle to be used for opening and closing the mobile nav

---
+++ app/components/Toggle.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useContext } from 'react';
+
+import { NavContext } from '../context/nav-context';
+
+const Toggle = () => {
+  const { toggle } = useContext(NavContext);
+
+  return (
+    <button
+      className='h-8 w-9 flex flex-col items-center justify-between py-1 z-[100] relative md:hidden'
+      onClick={toggle}
+    >
+      <span className='w-7 h-[3px] bg-black inline-block rounded-full' />
+      <span className='w-7 h-[3px] bg-black inline-block rounded-full' />
+      <span className='w-7 h-[3px] bg-black inline-block rounded-full' />
+    </button>
+  );
+};
+export default Toggle;
