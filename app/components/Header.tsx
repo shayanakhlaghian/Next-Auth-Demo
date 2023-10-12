3d55596afd40fc4194be0c2d728e8b08bc019307
Shayan <shayanakhlaghian@gmail.com>
2023-10-12 15:35:51 +0330

    add features to header
    add toggle and mobile nav for navigation on mobile size viewport to the header and wrapping heading with a link that directs the user to the homepage

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
+
 import Nav from './Nav';
+import MobileNav from './MobileNav';
+import Toggle from './Toggle';
 
 const Header = () => {
   return (
-    <header className='bg-slate-100 border-b border-gray-400 fixed top-0 left-0 shadow-md w-full h-16 flex flex-col items-center py-1 md:flex-row md:px-16 lg:px-20 md:items-center md:justify-between overflow-hidden'>
-      <h1 className='font-light text-xl md:text-2xl tracking-wide'>NAD</h1>
+    <header className='bg-slate-100 border-b border-gray-400 fixed top-0 left-0 shadow-md w-full h-16 flex items-center py-1 md:flex-row px-4 md:px-16 lg:px-20 md:items-center justify-between overflow-hidden z-[20]'>
+      <Link href='/'>
+        <h1 className='font-light text-xl md:text-2xl tracking-wide'>NAD</h1>
+      </Link>
       <Nav />
+      <Toggle />
+      <MobileNav />
     </header>
   );
 };
