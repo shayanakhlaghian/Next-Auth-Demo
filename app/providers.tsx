842fd035cbfbeeef7cba9bd86a65ac3b891c9cfd
Shayan <shayanakhlaghian@gmail.com>
2023-10-12 13:53:45 +0330

    add query client providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,14 @@
 'use client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 
+const queryClient = new QueryClient();
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryClientProvider>
+  );
 };
 export default Providers;
