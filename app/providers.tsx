492a8444bd4e9fb30e10af2ae02184eee5c59e58
Shayan <shayanakhlaghian@gmail.com>
2023-10-12 15:38:59 +0330

    add nav context provider to layout providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,16 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 
+import { NavProvider } from './context/nav-context';
+
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        <NavProvider>{children}</NavProvider>
+      </SessionProvider>
     </QueryClientProvider>
   );
 };
